403bb91c9c0f652e8fc93e1c7a4b8b474d0a9c76
AD-Archer <adarcher21@gmail.com>
2024-12-05 14:41:41 -0500

    push

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
+    // dialect: process.env.DB_DIALECT
 });
 
 connection.connect((err) => {
