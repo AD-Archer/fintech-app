386e10f5678ab14cbebf15af1f3c6716eba79336
AD-Archer <adarcher21@gmail.com>
2025-01-02 15:02:47 -0500

    Enhance CSS management and prevent caching during development
    - Updated server.js to set cache control headers for CSS files, preventing caching to ensure the latest styles are always loaded.
    - Modified index.ejs, landingpage.ejs, login.ejs, and register.ejs to append a timestamp to the CSS link for dynamic loading during development, improving the development experience.

--- server.js
+++ server.js
@@ -70,6 +70,10 @@ app.use(express.static('public', {
 app.use((req, res, next) => {
     if (req.url.endsWith('.css')) {
         res.setHeader('Content-Type', 'text/css');
+        // Prevent caching
+        res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        res.setHeader('Pragma', 'no-cache');
+        res.setHeader('Expires', '0');
     }
     next();
 });
